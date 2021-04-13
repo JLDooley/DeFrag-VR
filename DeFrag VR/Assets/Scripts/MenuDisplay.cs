a061785bcb2f0c1b410e87b32ea5aff0c9693808
JLDooley <jldooley1@gmail.com>
2021-04-13 22:41:14 +0100

    Created MenuDisplay script. Added Menu layer. Added MenuDOF Post Processing Profile. Implemented in Player prefab.

---
+++ DeFrag VR/Assets/Scripts/MenuDisplay.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Rendering.PostProcessing;
+
+namespace Game.Utility
+{
+    public class MenuDisplay : MonoBehaviour
+    {
+        [SerializeField]
+        private Camera menuCamera;
+
+        [SerializeField]
+        private float rampTime;
+
+        [SerializeField]
+        private AnimationCurve timeRampDown;
+
+        [SerializeField]
+        private PostProcessProfile menuProfile;
+        [SerializeField]
+        private AnimationCurve blurRampUp;
+
+
+        void Start()
+        {
+        
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+        
+        }
+
+        public IEnumerator DisplayMenu()
+        {
+
+        }
+    }
+}
+
